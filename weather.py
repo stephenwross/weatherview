b932a2fdf88256f8ce0ac122a134bba52be5161e
Firstname Lastname <email_address>
2017-12-12 21:49:45 -0500

    removed ffxx file

--- weather.py
+++ weather.py
@@ -4,28 +4,32 @@ import sys
 import configparser
 
 def main():
+    if len(sys.argv)>1:
+        loc=sys.argv[1]
+        print(getForecast(loc))
+    else:
+        print(getForecast())
+
+def getForecast(loc=""):
     config = configparser.ConfigParser()
     config.read("weather.cfg")
-    rloc=config.get("Defaults", "DefaultLoc")
-    rkey=config.get("Defaults",  "Key")
-    print(rloc)
-    if len(sys.argv)>1:
-        rloc=sys.argv[1]
-    getForecast(rloc,  rkey)
-
-def getForecast(loc,  key):
+    key = config.get("Defaults","Key")
+    if loc == "": loc = config.get("Defaults","DefaultLoc")
     f = urllib.request.urlopen('http://api.wunderground.com/api/'+key+'/geolookup/forecast10day/q/'+loc+'.json')
     json_string = f.read()
     parsed_json = json.loads(json_string)
-    location = parsed_json['location']['city']+', '+parsed_json['location']['state']
-    print("10-Day forecast for "+location+":")
+    try:
+        location = parsed_json['location']['city']+', '+parsed_json['location']['state']
+    except:
+        return "I don't know where "+loc+" is!"
+    forecast = '10-Day forecast for ' + location + ':\n'
     for x in range(0, 9):
-        print(
+        forecast += (
         parsed_json['forecast']['simpleforecast']['forecastday'][x]['date']['weekday_short']+': '+
         parsed_json['forecast']['simpleforecast']['forecastday'][x]['high']['fahrenheit']+'/'+
         parsed_json['forecast']['simpleforecast']['forecastday'][x]['low']['fahrenheit']+ ' -- '+
-        parsed_json['forecast']['simpleforecast']['forecastday'][x]['conditions']
-        )
+        parsed_json['forecast']['simpleforecast']['forecastday'][x]['conditions']+'\n')
+    return forecast
     f.close()
 
 if __name__ == "__main__":
